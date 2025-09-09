1c36d2e14e6534c22635221ea056bda8c60508b8
joevalen2033 <joevalen@bancolombia.com.co>
2025-09-09 09:25:39 -0500

    Taller Completado

---
+++ TallerJdbc/src/Conexion/DBconexion.java
@@ -0,0 +1,25 @@
+package Conexion;
+
+import java.sql.*;
+
+public class DBconexion {
+
+    public static final String URL = "jdbc:mysql://localhost:3306/bd_estudiantes";
+    public static final String USER = "root";
+    public static final String PASSWORD = "Weyder26";
+
+
+    public static Connection conectar() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+}
